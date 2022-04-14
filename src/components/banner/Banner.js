d2fa27e59df9731894b4013a1ff3b5c225f5e07f
draganvasilj52 <80092914+draganvasilj52@users.noreply.github.com>
2022-04-14 11:59:14 +0200

    second commit

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,12 +1,25 @@
 import React, { useState } from 'react'
 import image1 from '../../assets/today-offer.jpg'
 import image2 from '../../assets/ebay.jpg'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
 
 let count = 0
 const Banner = () => {
   const featuredProducts = [
-    { title: 'Title1', path: image1 },
-    { title: 'Title2', path: image2 },
+    {
+      title: 'You may have a special offer',
+      message: 'Sign in to your account to find out',
+      path: image1,
+      buttonText: 'Go to My Ebay',
+    },
+    {
+      title: 'Everyone says these sellers are the best',
+      message:
+        'These items come with outstanding service from Top Rated Sellers',
+      path: image2,
+      buttonText: 'Shop the best',
+    },
   ]
 
   const [currentIndex, setCurrentIndex] = useState(0)
@@ -22,17 +35,58 @@ const Banner = () => {
   }
 
   return (
-    <div className="flex relative">
-      {featuredProducts.map((item) => (
-        <>
-          <div className="w-4/12 bg-red-700">{item[currentIndex].title}</div>
-          <img className="w-8/12 h-80" src={item[currentIndex].path} alt="" />
-          <div className="absolute w-full top-1/2 px-3 flex justify-between items-center">
-            <button onClick={handleOnPrevClick}>Previous</button>
-            <button onClick={handleOnNextClick}>Next</button>
-          </div>
-        </>
-      ))}
+    <div className="flex relative container mx-auto my-0">
+      {featuredProducts.map(
+        (item, index) =>
+          index === currentIndex && (
+            <>
+              <div
+                className={
+                  currentIndex === 0
+                    ? ' w-2/5 bg-red-700 p-8 space-y-6'
+                    : ' w-2/5 p-8 space-y-6 bg-blue-700'
+                }
+              >
+                <h2
+                  className={
+                    currentIndex === 0
+                      ? ' text-orange-300 text-4xl'
+                      : 'text-blue-300 text-4xl'
+                  }
+                >
+                  {item.title}
+                </h2>
+                <h3
+                  className={
+                    currentIndex === 0
+                      ? ' text-orange-300 text-base'
+                      : 'text-blue-300 text-base'
+                  }
+                >
+                  {item.message}
+                </h3>
+                <button
+                  className={
+                    currentIndex === 0
+                      ? 'w-4/12 text-orange-300 border-2 border-orange-300 border-solid'
+                      : 'w-4/12 text-blue-300 border-2 border-blue-300 border-solid'
+                  }
+                >
+                  {item.buttonText}
+                </button>
+              </div>
+              <img className="w-8/12 h-80" src={item.path} alt="banner" />
+              <div className="absolute w-full top-1/2 px-3 flex justify-between items-center">
+                <button onClick={handleOnPrevClick}>
+                  <ArrowBackIosIcon />
+                </button>
+                <button onClick={handleOnNextClick}>
+                  <ArrowForwardIosIcon />
+                </button>
+              </div>
+            </>
+          )
+      )}
     </div>
   )
 }
