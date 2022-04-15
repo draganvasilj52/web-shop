5a1ec3f4712d456ea79e91a4634cef607d291532
draganvasilj52 <80092914+draganvasilj52@users.noreply.github.com>
2022-04-15 11:48:58 +0200

    ten commit

--- src/components/common/Dropdown.js
+++ src/components/common/Dropdown.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import { useState } from 'react'
-const Dropdown = ({ title, spanProp, containerProp, categories }) => {
+const Dropdown = ({
+  title,
+  spanProp,
+  containerProp,
+  categories,
+  categoriesContainerProp,
+  categoriesItemProp,
+}) => {
   const [isActive, setIsActive] = useState(false)
   return (
     <div
-      className={`relative ${containerProp}`}
+      className={`relative  ${containerProp}`}
       onClick={() => setIsActive(!isActive)}
     >
       <div className="flex items-center">
         <span className={`${spanProp}`}>{title}</span> <KeyboardArrowDownIcon />
       </div>
       {isActive && (
-        <div className="flex flex-wrap w-96 absolute top-full  z-10 bg-white border-2 border-solid border-slate-200">
+        <div className={`${categoriesContainerProp}`}>
           {categories.map((item, index) => (
-            <div className="h-auto w-1/3" key={index}>
+            <div className={` ${categoriesItemProp}`} key={index}>
               {item}
             </div>
           ))}
--- src/components/header/headerMiddle/HeaderMiddle.js
+++ src/components/header/headerMiddle/HeaderMiddle.js
@@ -1,6 +1,5 @@
 import logo from '../../../assets/slika.png'
 import SearchIcon from '@mui/icons-material/Search'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import Dropdown from '../../common/Dropdown'
 import { useSelector } from 'react-redux'
 const HeaderMiddle = () => {
@@ -14,6 +13,8 @@ const HeaderMiddle = () => {
         title="Shop by category"
         spanProp="pr-1 leading-4 hover:text-blue-600 "
         containerProp="text-sm h-12 w-24 flex items-center mx-1.5 p-2 hover:cursor-pointer "
+        categoriesContainerProp="flex flex-wrap absolute top-full  z-10 bg-white border-2 border-solid border-slate-200"
+        categoriesItemProp="h-28 basis-2/6  shrink-0 grow-0 w-96"
       />
 
       <div className="w-2/4 relative flex items-center text-gray-500 focus-within:text-black border-2 border-solid border-black border-r border-r-gray-300">
@@ -27,10 +28,14 @@ const HeaderMiddle = () => {
           placeholder="Search for anything"
         />
       </div>
-      <div className="text-xs flex items-center justify-between px-4 w-2/12 border-solid border-black border-y-2 border-r-2 border-l-0">
-        <span>All Categories</span>
-        <KeyboardArrowDownIcon className="text-gray-400" />
-      </div>
+
+      <Dropdown
+        categories={categories}
+        title="All Categories"
+        containerProp="text-xs flex items-center justify-between px-4 w-2/12 border-solid border-black border-y-2 border-r-2 border-l-0"
+        categoriesContainerProp="flex flex-col w-full flex-wrap absolute top-full  z-10 bg-white border-2 border-solid border-slate-200"
+        categoriesItemProp=""
+      />
       <button className="cursor-pointer ml-1.5 py-2 px-4 bg-blue-600 text-white w-40">
         Search
       </button>
